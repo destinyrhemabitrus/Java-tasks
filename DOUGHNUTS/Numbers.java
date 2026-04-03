5babeabcaf6c7a265f4665e6d4c535b3e7942a06
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-03 21:11:06 +0100

    Adding Numbers.java file

---
+++ DOUGHNUTS/Numbers.java
@@ -0,0 +1,15 @@
+//Question 3: A program that Prints 5 different numbers horizontally e.g 35, 44, 56, 89, 2
+
+public class Numbers{
+
+	public static void main(String[] args){
+
+		System.out.print("35 ");
+		System.out.print("44 ");
+		System.out.print("56 ");
+		System.out.print("89 ");
+		System.out.print("2");
+
+	}
+
+}
