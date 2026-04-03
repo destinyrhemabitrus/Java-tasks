64643e1c832139a0f3875c0c5938acbdd0d90c74
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-03 22:12:02 +0100

    Adding Nigeria.java file

---
+++ DOUGHNUTS/Nigeria.java
@@ -0,0 +1,9 @@
+//Question 5: A program that prints a statement
+public class Nigeria{
+
+	public static void main(String[] args){
+		System.out.printf("Nigeria became independent in %d and is full of great minds. i am of them, My name is %s and i weigh %d kg and i am a %c", 1960, "Rhema Destiny",25, 'M');
+
+	}
+
+}
