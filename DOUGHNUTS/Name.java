9be722a3342ed20843dabefadfb108b6657500d7
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-03 21:03:39 +0100

    adding Name.java file

---
+++ DOUGHNUTS/Name.java
@@ -0,0 +1,11 @@
+//Question 2: A program that Prints first name, last name and this year on separate lines using only Print statement
+
+public class Name{
+
+	public static void main(String[] args){
+
+		System.out.print("Destiny\nRhema\n2026");
+
+	}
+
+}
