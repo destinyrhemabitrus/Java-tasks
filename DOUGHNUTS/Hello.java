ac4b6743c710dfa7a5fbe02976f0da1d3f30ca3b
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-03 22:09:09 +0100

    Adding Hello.java file

---
+++ DOUGHNUTS/Hello.java
@@ -0,0 +1,11 @@
+//Question 4: A program that displays and stores a name in a variable and print Hello and Name
+
+public class Hello{
+
+	public static void main(String[] args){
+		String name = "Rhema";
+		System.out.printf("Hello, %s", name);
+
+	}
+
+}
