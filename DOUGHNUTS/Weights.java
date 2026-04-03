b48a2b018fd82f8e13ce6d698a5ae4dcb121b525
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-03 22:14:48 +0100

    Adding Weights.java file

---
+++ DOUGHNUTS/Weights.java
@@ -0,0 +1,16 @@
+//Question 5: Declare and store five weights and print them vertically 35.4kg, 45.7kg, 45.9kg, 102.0kg, 40.23kg
+public class Weights{
+
+	public static void main(String[] args){
+	
+		double weightOne = 35.4;
+		double weightTwo = 45.7;
+		double weightThree = 45.9;
+		double weightFour = 102.0;
+		double weightFive = 40.23;
+
+		System.out.print(weightOne + "kg" + "\n" + weightTwo + "kg" + "\n" + weightThree + "kg" + "\n" + weightFour + "kg" + "\n" + weightFive + "kg");
+
+	}
+
+}
