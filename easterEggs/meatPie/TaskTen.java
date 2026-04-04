3929c49eb5624b19e14360a30996cc34050360a8
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:55:04 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskTen.java
@@ -0,0 +1,17 @@
+// Write a program that takes a number and prints its double
+
+
+import java.util.Scanner;
+public class TaskTen{
+
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		double num = input.nextInt();
+
+		System.out.printf("Result: %f", num);
+		 
+	}
+
+}
