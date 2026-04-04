2779eddf377b560ee8cae53d2516b82e0905ac0f
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 09:02:02 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskTwentyNine.java
@@ -0,0 +1,12 @@
+// A program that prints the phrase she said "Hello!" including the double quote in the output
+
+import java.util.Scanner;
+public class TaskTwentyNine{
+
+	public static void main(String[] args){		
+
+		System.out.println("She said, \"Hello!\"");
+		 
+	}
+
+}
