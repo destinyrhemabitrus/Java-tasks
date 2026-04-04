e682f8faeecb8dc330cae4f47e668ab4bb4f0d60
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 09:00:31 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskTwentyFive.java
@@ -0,0 +1,11 @@
+// A program that prints three blank line between two messages: START and END
+
+public class TaskTwentyFive{
+
+	public static void main(String[] args){		
+
+		System.out.print("START\n\n\nEND");
+				 
+	}
+
+}
