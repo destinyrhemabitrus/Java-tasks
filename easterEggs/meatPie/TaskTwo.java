6e89f587f16fdf55a7c2717467f28029fa9601c0
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:51:25 +0100

    Add Task2 file

---
+++ easterEggs/meatPie/TaskTwo.java
@@ -0,0 +1,15 @@
+//A program that asks for your age and prints it
+
+import java.util.Scanner;
+public class TaskTwo{
+
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("How old are you: ");
+		int age = input.nextInt();
+		System.out.printf("You are %d years old", age);
+
+	}
+
+}
