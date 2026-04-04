6a382ca80a43223dfb8fedaa160e6e70b88be2db
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:56:10 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskThirteen.java
@@ -0,0 +1,14 @@
+// A program that declares a string variable called city and print a statement with it
+
+public class TaskThirteen{
+
+	public static void main(String[] args){
+		
+		String city;
+		city = "Lagos";
+
+		System.out.printf("I live in %s", city);
+		 
+	}
+
+}
