bd83c6e59bc079e043c97aab22912ff1e0cb741b
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:59:48 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskTwentyThree.java
@@ -0,0 +1,13 @@
+// A program that concatenates first name and last name
+
+public class TaskTwentyThree{
+
+	public static void main(String[] args){		
+
+		String firstName = "Lucky";
+		String lastName = "Adams";
+
+		System.out.print(firstName + lastName);		 
+	}
+
+}
