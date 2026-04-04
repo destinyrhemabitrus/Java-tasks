7a2fcf6753e366a06e2ed1a5b6d48d9e2ae994b7
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:47:28 +0100

    Add TaskOne.java file

---
+++ easterEggs/meatPie/TaskOne.java
@@ -0,0 +1,14 @@
+// A program that print a right angle triangle of stars
+
+public class TaskOne{
+
+	public static void main(String[] args){
+			
+		System.out.println("*");
+		System.out.println("* * ");
+		System.out.println("* * *");
+		System.out.print("* * * *");
+				 
+	}
+
+}
