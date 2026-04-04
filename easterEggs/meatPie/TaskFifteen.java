93cfc85b0958bb1a576e198ffcea97bb2a118128
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 08:56:54 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskFifteen.java
@@ -0,0 +1,14 @@
+// A program that print a box made of asterisks- 4 rows each with 4 stars
+
+public class TaskFifteen{
+
+	public static void main(String[] args){
+			
+		System.out.println("* * * *");
+		System.out.println("* * * *");
+		System.out.println("* * * *");
+		System.out.print("* * * *");
+				 
+	}
+
+}
