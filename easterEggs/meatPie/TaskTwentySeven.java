9eb60024aba87fc80c6dc92b82718a6c2d7dca7b
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-04 09:01:22 +0100

    Add Task file

---
+++ easterEggs/meatPie/TaskTwentySeven.java
@@ -0,0 +1,14 @@
+// A program that declares int marks = 78 and then cast it to a double and print both the original int and the cast double value
+
+public class TaskTwentySeven{
+
+	public static void main(String[] args){
+		
+		int marks = 78;
+		double marks2 = marks; 		
+
+		System.out.printf("Original value = %d, casted value = %f", marks, marks2);
+		 
+	}
+
+}
