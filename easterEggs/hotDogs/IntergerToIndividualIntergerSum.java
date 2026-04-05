3f97ce6124f7c7086145deb484e9d3c623515ce9
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-05 14:46:59 +0100

    Java Task

---
+++ easterEggs/hotDogs/IntergerToIndividualIntergerSum.java
@@ -0,0 +1,24 @@
+//Quetion 7
+import java.util.Scanner;
+
+public class IntergerToIndividualIntergerSum{
+
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Enter a number: ");
+	int num = input.nextInt();
+
+	int wholeNum1 = num/10;
+	int remainder1 = num%10;	
+	
+	int wholeNum2 = wholeNum1/10;
+	int remainder2 = wholeNum1%10;
+
+	int sum = wholeNum2 + remainder2 + remainder1;
+
+	System.out.printf("Integer %d sum is: %d", num, sum);
+
+	}
+}
