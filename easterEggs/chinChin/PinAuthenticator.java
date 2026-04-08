ee9c7c4752b2016b90c849888c4dd59308efe4f7
Rhema Destiny <destinyrhemabitrus@gmail.com>
2026-04-08 06:30:52 +0100

    Java Task

---
+++ easterEggs/chinChin/PinAuthenticator.java
@@ -0,0 +1,24 @@
+//Quetion 24
+
+import java.util.Scanner;
+
+public class PinAuthenticator{
+
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Enter your 4 digit PIN: ");
+	int pin = input.nextInt();
+	
+	int setupPin = 1234;
+	int balance =1000;
+
+	if(pin==setupPin){
+		System.out.printf("Balance: $%d", balance);
+		} else {
+		System.out.print("Incorrect PIN");
+		
+		}
+	}
+}
